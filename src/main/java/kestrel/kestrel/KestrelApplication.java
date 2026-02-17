b949a8c061abae2758da36bfa53fa6cf5e950cbf
GirTabAleph <nineteencycles@gmail.com>
2026-02-16 18:34:02 -0600

    Initial project structure

---
+++ src/main/java/kestrel/kestrel/KestrelApplication.java
@@ -0,0 +1,13 @@
+package kestrel.kestrel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KestrelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KestrelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kestrel/kestrel/KestrelApplicationTests.java
@@ -0,0 +1,13 @@
+package kestrel.kestrel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KestrelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
